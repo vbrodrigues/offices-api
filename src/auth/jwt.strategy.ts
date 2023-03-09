98ef8ba4b5a40ba3f1c97e329f0183a04535f5bc
Vitor Rodrigues <vitorborbarodrigues@gmail.com>
2023-03-08 23:07:57 -0300

    add office_id validation to all routes

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,16 +1,31 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private moduleRef: ModuleRef) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.SECRET,
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: any) {
+  async validate(request, payload: any) {
+    if (request.body.office_id) {
+      if (request.body.office_id !== payload.office_id) {
+        throw new UnauthorizedException();
+      }
+    }
+
+    if (request.params.id) {
+      if (request.params.id !== payload.office_id) {
+        throw new UnauthorizedException();
+      }
+    }
+
     return {
       employee_id: payload.sub,
       office_id: payload.office_id,
