237f51eb61bc80e287f3a111d520f88792a1b29b
Vítor Rodrigues <vitorborbarodrigues@gmail.com>
2023-03-28 00:47:26 -0300

    feat: add AWS Cloudfront URL sign for S3 files

--- office-api/src/app/client/usecases/find-client.usecase.ts
+++ office-api/src/app/client/usecases/find-client.usecase.ts
@@ -1,10 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Client } from '@prisma/client';
+import { StorageService } from 'src/providers/storage/storage';
 import { ClientsRepository } from '../client.repository';
 
 @Injectable()
 export class FindClientUsecase {
-  constructor(private clientsRepository: ClientsRepository) {}
+  constructor(
+    private clientsRepository: ClientsRepository,
+    private storageService: StorageService,
+  ) {}
 
   async execute(office_id, client_id: string): Promise<Client | null> {
     const client = await this.clientsRepository.findById(client_id);
@@ -16,6 +20,8 @@ export class FindClientUsecase {
     if (office_id !== client.office_id) {
       throw new UnauthorizedException();
     }
+
+    client.logo = await this.storageService.signFile(client.logo);
 
     return client;
   }
--- office-api/src/providers/storage/aws-s3.ts
+++ office-api/src/providers/storage/aws-s3.ts
@@ -1,10 +1,20 @@
 import * as AWS from 'aws-sdk';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cf = require('aws-cloudfront-sign');
+
 import { StorageService } from './storage';
 
 export class S3StorageService implements StorageService {
   s3: AWS.S3;
+  cloudfrontDNS: string;
+  signingParams: any;
   constructor() {
     this.s3 = new AWS.S3();
+    this.signingParams = {
+      keypairId: process.env.AWS_CLOUDFRONT_KEYPAIR_ID,
+      privateKeyPath: process.env.AWS_CLOUDFRONT_KEY_PATH,
+      keyGroupId: process.env.AWS_CLOUDFRONT_KEY_GROUP_ID,
+    };
   }
 
   async uploadFile(file: Buffer, filepath: string): Promise<string> {
@@ -29,5 +39,15 @@ export class S3StorageService implements StorageService {
       return false;
     }
     return true;
+  }
+
+  async signFile(filepath: string): Promise<string> {
+    console.log(this.signingParams);
+    return cf.getSignedUrl(
+      `${process.env.AWS_CLOUDFRONT_DNS}/${
+        filepath.split('amazonaws.com/')[1]
+      }`,
+      this.signingParams,
+    );
   }
 }
